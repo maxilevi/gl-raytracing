229c147a1024bcdeec76c297e4e70d9078bef8b5
Zaphyk <zaphyk@live.com>
2018-07-16 20:43:25 -0300

    basic VAO

--- rendering/VAO.cpp
+++ rendering/VAO.cpp
@@ -4,22 +4,51 @@
 
 #include "VAO.h"
 
-VAO::VAO()
+VAO::VAO(VBO** buffers, int count)
 {
+    this->_count = count;
+    this->_buffers = buffers;
     glCreateVertexArrays(1, &this->_id);
+    this->Bind();
+    GLuint i = 0;
+    for(VBO* buffer = *buffers; buffer < *buffers + count; buffer++)
+    {
+        buffer->Bind();
+        glVertexAttribPointer(i, buffer->getSize(), buffer->getPointerType(), GL_FALSE, 0, 0);
+        i++;
+    }
+    this->Unbind();
 }
 
 void VAO::Draw() const
 {
-
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 }
 
 void VAO::Bind() const
 {
-    //glEnableVertexAttribArray();
+    glBindVertexArray(this->_id);
+    for(GLuint i = 0; i < this->_count; i++)
+    {
+        glEnableVertexAttribArray(i);
+    }
+}
+
+void VAO::Unbind() const
+{
+    glBindVertexArray(0);
+    for(GLuint i = 0; i < this->_count; i++)
+    {
+        glDisableVertexAttribArray(i);
+    }
 }
 
 VAO::~VAO()
 {
-    glDeleteVertexArrays(this._id);
+    glDeleteVertexArrays(1, &this->_id);
+    for(VBO* buffer = *this->_buffers; buffer < *this->_buffers + this->_count; buffer++)
+    {
+        delete *buffer;
+    }
+    delete this->_count;
 }
--- rendering/VAO.h
+++ rendering/VAO.h
@@ -7,18 +7,26 @@
 
 
 #include <GL/glew.h>
+#include "VBO.h"
 
 class VAO {
 
-private:
+protected:
     GLuint _id;
+    int _count;
+    VBO** _buffers;
 public:
-    VAO();
+    VAO(VBO** buffers, int count);
     ~VAO();
     void Bind() const;
     void Unbind() const;
     void Draw() const;
 };
 
-
+template< class T1 >
+class VAO<T1> : VAO {
+public:
+    VAO(VBO<T1> Buffer1);
+    ~VAO();
+};
 #endif //GLRAYTRACING_VAO_H
